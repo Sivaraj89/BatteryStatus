f452d342c9e788dd6efe7c26cacd51a68c1bda63
Seenu <>
2018-07-06 10:36:33 +0530

    added track

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,26 +7,57 @@
 //
 
 import UIKit
-import FirebaseStorage
-
 
 class ViewController: UIViewController, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate{
-
-
+    
+    @IBOutlet var trackButton:UIButton!
+    @IBOutlet var messageLabel:UILabel!
+    
+    let startTitle = "Start Tracking"
+    let stopTitle = "Stop Tracking"
+    var trackStatus = false
+    var trackTimer:Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
-        let loop = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.batteryStatus), userInfo: nil, repeats: true)
-        RunLoop.current.add(loop, forMode: .commonModes)
+        self.trackButton.setTitle(startTitle, for: UIControlState.normal)
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-        let Alert = UIAlertController(title: "Alert", message: "plz open target application", preferredStyle: UIAlertControllerStyle.alert)
-        self.present(Alert, animated: true, completion: nil)
     }
-
-  @objc func batteryStatus(){
+    
+    @IBAction func trackButtonAction(sender:UIButton) {
+        if !trackStatus {
+            trackStatus = true
+            self.startTracking()
+        } else {
+            trackStatus = false
+            self.stopTracking()
+        }
+    }
+    
+    func startTracking() {
+        self.trackButton.setTitle(stopTitle, for: UIControlState.normal)
+        
+        if trackTimer == nil {
+            UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
+            trackTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.batteryStatus), userInfo: nil, repeats: true)
+            RunLoop.current.add(trackTimer!, forMode: .commonModes)
+            
+            let Alert = UIAlertController(title: "Alert", message: "plz open target application to track battery status", preferredStyle: UIAlertControllerStyle.alert)
+            self.present(Alert, animated: true, completion: nil)
+        }
+    }
+    
+    func stopTracking() {
+        self.trackButton.setTitle(startTitle, for: UIControlState.normal)
+        
+        if trackTimer != nil {
+            trackTimer?.invalidate()
+            trackTimer = nil
+        }
+    }
+    
+    @objc func batteryStatus(){
         let myDevice = UIDevice.current
         myDevice.isBatteryMonitoringEnabled = true
         let batLeft: Float = myDevice.batteryLevel
@@ -35,45 +66,43 @@ class ViewController: UIViewController, URLSessionDelegate, URLSessionTaskDelega
         let dateFormatter : DateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm:ss"
         let date = Date()
-        let dateString = dateFormatter.string(from: date) 
+        let dateString = dateFormatter.string(from: date)
         print(batinfo,dateString)
         
-                if let filepath = Bundle.main.path(forResource: "data", ofType: "json") {
-                    do {
-                        let contents = try String(contentsOfFile: filepath)
-                        let data = contents.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-                        do {
-                            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
-                            var andict:Dictionary = json
-                            var dict1 = andict["per_ip_data"] as! Dictionary<String, AnyObject>
-                            var dict2 = dict1["section_data"] as! Array<AnyObject>
-                            var batteryObj = dict2[0] as! [String:AnyObject]
-                            batteryObj["battery"] = batinfo
-                            dict2[0] = batteryObj as AnyObject
-                            dict1["section_data"] = dict2 as AnyObject
-                            andict["per_ip_data"] = dict1 as AnyObject
-                            dict1 = andict
-                            print(andict)
-                            let jsonData = try? JSONSerialization.data(withJSONObject: andict, options: [])
-                            let jsonString = String(data: jsonData!, encoding: .utf8)
-                            let datas: Data = NSKeyedArchiver.archivedData(withRootObject: andict)
-                            var ref: DatabaseReference!
-                            ref = Database.database().reference()
-                            
-                            let request = NSMutableURLRequest(url:NSURL(string: "http://54.164.87.105:8080/networkmonitoring-6-Apr-17/appdata")! as URL)
-                            request.httpMethod = "POST"
-                            request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
-                            uploadFiles(request: request, data: jsonData as! NSData)
-                        } catch let error as NSError {
-                            print("Failed to load: \(error.localizedDescription)")
-                        }
-
-                    } catch {
-                        print("Fetched")
-                    }
-                } else {
-                    print("not found")
+        if let filepath = Bundle.main.path(forResource: "data", ofType: "json") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                let data = contents.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+                    var andict:Dictionary = json
+                    var dict1 = andict["per_ip_data"] as! Dictionary<String, AnyObject>
+                    var dict2 = dict1["section_data"] as! Array<AnyObject>
+                    var batteryObj = dict2[0] as! [String:AnyObject]
+                    batteryObj["battery"] = batinfo
+                    dict2[0] = batteryObj as AnyObject
+                    dict1["section_data"] = dict2 as AnyObject
+                    andict["per_ip_data"] = dict1 as AnyObject
+                    dict1 = andict
+                    print(andict)
+                    let jsonData = try? JSONSerialization.data(withJSONObject: andict, options: [])
+                    let jsonString = String(data: jsonData!, encoding: .utf8)
+                    let datas: Data = NSKeyedArchiver.archivedData(withRootObject: andict)
+                    
+                    let request = NSMutableURLRequest(url:NSURL(string: "http://54.164.87.105:8080/networkmonitoring-6-Apr-17/appdata")! as URL)
+                    request.httpMethod = "POST"
+                    request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+                    uploadFiles(request: request, data: jsonData as! NSData)
+                } catch let error as NSError {
+                    print("Failed to load: \(error.localizedDescription)")
                 }
+                
+            } catch {
+                print("Fetched")
+            }
+        } else {
+            print("not found")
+        }
         
         
         
@@ -112,7 +141,7 @@ class ViewController: UIViewController, URLSessionDelegate, URLSessionTaskDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
